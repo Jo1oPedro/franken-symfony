adcb6e9fa8e832a9e126e3a060e263e7a945f1c0
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-05-29 00:54:50 -0300

    refactor: add genereric return to transaction manager interface callback

--- api/src/shared/Port/TransactionManagerInterface.php
+++ api/src/shared/Port/TransactionManagerInterface.php
@@ -4,5 +4,10 @@ namespace App\shared\Port;
 
 interface TransactionManagerInterface
 {
+    /**
+     * @template T
+     * @param callable(): T $callback
+     * @return T
+     */
     public function transactional(callable $callback): mixed;
 }
