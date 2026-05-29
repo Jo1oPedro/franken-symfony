4031260d4961d1add0a84b41deab875265968d56
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-05-29 00:29:41 -0300

    feat: add phpstan fixes on user repository

--- api/src/Identity/Repository/UserRepository.php
+++ api/src/Identity/Repository/UserRepository.php
@@ -6,6 +6,9 @@ use App\Identity\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * @extends ServiceEntityRepository<User>
+ */
 final class UserRepository extends ServiceEntityRepository implements UserRepositoryInterface
 {
     public function __construct(
